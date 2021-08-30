55681e74eae7097a9344589ee3632b7ebdcc259c
mattmoyka <mmoyka17@gmail.com>
2021-08-30 17:01:44 -0400

    began news api, finished axios request

--- script.js
+++ script.js
@@ -61,37 +61,70 @@ searchZipButton.addEventListener('click', (event) => {
 
 async function getWeather() {
   BASE_URL = `http://api.weatherapi.com/v1/forecast.json?key=2d30ac272187439b8e1183146213008&q=${zip_code}&days=1&aqi=no&alerts=no`
-  let weather = await axios.get(BASE_URL);
-  console.log(weather.data);
-  let currentWeather = weather.data.forecast.forecastday[0].hour;
-  console.log(weather.data.forecast.forecastday[0].day.uv);
+  try {
+    let weather = await axios.get(BASE_URL);
+    console.log(weather.data);
+    let currentWeather = weather.data.forecast.forecastday[0].hour;
+    console.log(weather.data.forecast.forecastday[0].day.uv);
 
-  let today = new Date();
-  let time = today.getHours();
+    let today = new Date();
+    let time = today.getHours();
 
 
-  let temperature = document.createElement('div')
-  temperature.classList.add('temp');
-  temperature.innerText = `Current temperature: ${currentWeather[time].temp_f}F`;
-  weatherDiv.appendChild(temperature);
+    let temperature = document.createElement('div')
+    temperature.classList.add('temp');
+    temperature.innerText = `Current temperature: ${currentWeather[time].temp_f}F`;
+    weatherDiv.appendChild(temperature);
 
-  let uvIndex = document.createElement('div');
-  uvIndex.classList.add('uv');
-  uvIndex.innerText = `UV index: ${currentWeather[time].uv} `;
-  weatherDiv.appendChild(uvIndex);
+    let uvIndex = document.createElement('div');
+    uvIndex.classList.add('uv');
+    uvIndex.innerText = `UV index: ${currentWeather[time].uv} `;
+    weatherDiv.appendChild(uvIndex);
 
-  let rainChance = document.createElement('div');
-  rainChance.classList.add('rainChance');
-  rainChance.innerText = `Chance of rain: ${currentWeather[time].chance_of_rain}`;
-  weatherDiv.appendChild(rainChance);
+    let rainChance = document.createElement('div');
+    rainChance.classList.add('rainChance');
+    rainChance.innerText = `Chance of rain: ${currentWeather[time].chance_of_rain}`;
+    weatherDiv.appendChild(rainChance);
 
-  let forecast = document.createElement('img');
-  forecast.classList.add('forecastImg');
-  forecast.src = currentWeather[time].condition.icon;
-  weatherDiv.appendChild(forecast);
+    let forecast = document.createElement('img');
+    forecast.classList.add('forecastImg');
+    forecast.src = currentWeather[time].condition.icon;
+    weatherDiv.appendChild(forecast);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //==================================================================================
 
 //News API
+const NEWS_API_KEY = '1941ec12d2054d399f9949a853bd47b6';
+let NEWS_SEARCH = "";
+let News_Api = `https://newsapi.org/v2/everything?q=${NEWS_SEARCH}&from=2021-08-30&sortBy=popularity&apiKey=${NEWS_API_KEY}`;
 
+
+let searchNewsButton = document.querySelector('#searchNewsButton');
+let newsDiv = document.querySelector('.news_article');
+let searchNews = document.querySelector('#searchNews');
+
+searchNewsButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  newsDiv.innerHTML = "";
+  console.log(searchNews.value);
+  NEWS_SEARCH = searchNews.value;
+  getNews();
+  searchNews.value = "";
+})
+
+async function getNews() {
+  News_Api = `https://newsapi.org/v2/everything?q=${NEWS_SEARCH}&from=2021-08-30&sortBy=popularity&apiKey=${NEWS_API_KEY}`
+  try {
+    let newsData = await axios.get(News_Api);
+    let newsArticleData = newsData.data.articles;
+    console.log(newsArticleData);
+
+    //figure out how to display articles
+  } catch (error) {
+    console.log(error);
+  };
+};
